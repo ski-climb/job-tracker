bd2681092eaf7048ec09a8cc00fe44d7aaa5475f
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-15 15:16:48 -0700

    Error messages for creating a new company

--- spec/features/companies/user_creates_new_company_spec.rb
+++ spec/features/companies/user_creates_new_company_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 describe "User creates a new company" do
+  scenario "a user sees helpful error messages and nothing is saved to the database when no name provided" do
+    visit new_company_path
+
+    fill_in "company[name]", with: ""
+    click_button "Create"
+
+    expect(page).to have_content "Name can't be blank"
+    expect(Company.count).to eq(0)
+  end
+
+  scenario "a user sees helpful error messages and nothing is saved to the database when a duplicate name is  provided" do
+    Company.create(name: "ESPN")
+    visit new_company_path
+
+    fill_in "company[name]", with: "ESPN"
+    click_button "Create"
+
+    expect(page).to have_content "Name has already been taken"
+    expect(Company.count).to eq(1)
+  end
+
   scenario "a user can create a new company" do
     visit new_company_path
 
