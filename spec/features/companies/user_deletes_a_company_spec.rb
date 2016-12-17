eba7016596e16bf929904cbe59eed1a243e0d8a4
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-17 12:43:52 -0700

    Delete a company with jobs and all associated jobs

--- spec/features/companies/user_deletes_a_company_spec.rb
+++ spec/features/companies/user_deletes_a_company_spec.rb
@@ -9,6 +9,26 @@ describe "User deletes existing company" do
       click_link "Delete"
     end
 
-    expect(page).to have_content("#{company.name} was successfully deleted!")
+    expect(page).to have_content("#{company.name} and all associated jobs for #{company.name} were successfully deleted!")
+    expect(Company.count).to eq 0
+  end
+
+  scenario "a user can delete a company that has jobs" do
+    company = create(:company)
+    create_list(:job, 2, company: company)
+    visit companies_path
+
+    expect(Job.count).to eq 2
+    expect(Company.count).to eq 1
+    expect(company.jobs.count).to eq 2
+
+    within "#company_#{company.id}" do
+      click_on 'Delete'
+    end
+
+    expect(page).to have_content("#{company.name} and all associated jobs for #{company.name} were successfully deleted!")
+    expect(Job.count).to eq 0
+    expect(Company.count).to eq 0
+    expect(company.jobs.count).to eq 0
   end
 end
