c74135bd979f553d9b8d20cd662d515819c911cd
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-17 20:19:33 -0700

    Deleting a job deletes its comments as well

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -14,4 +14,20 @@ describe "User deletes existing job" do
     expect(Job.pluck(:title)).not_to include job_2.title
     expect(Job.count).to eq 2
   end
+
+  scenario "a user can delete a job that has comments" do
+    company = create(:company)
+    job_1, job_2, job_3 = create_list(:job, 3, company: company)
+    create_list(:comment, 3, job: job_2)
+    visit company_jobs_path(company)
+
+    within "#job_#{job_2.id}" do
+      click_on "Delete"
+    end
+
+    expect(page).to have_content "#{job_2.title} at #{company.name} was successfully deleted!"
+    expect(Job.pluck(:title)).not_to include job_2.title
+    expect(Job.count).to eq 2
+    expect(Comment.count).to eq 0
+  end
 end
