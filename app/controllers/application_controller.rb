cbd6ad6283e49aa806ec36e68e73d1b32738a260
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-18 09:05:05 -0700

    Add http basic auth for a tiny bit of security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: "job_tracker", password: "DvzCApGtnapunUgRgBZy3BF4iYNbdkhp"
 
   private
 
